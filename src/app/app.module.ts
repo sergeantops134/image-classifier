c3dfb4c6bad13cc0b95dafaff38e648ef80fe4c5
PavloOlenych <Pavlo.Olenych@oneenterprise.com>
2023-01-31 23:14:14 +0200

    Base skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ImageClassifierModule} from "./image-classifier/image-classifier.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ImageClassifierModule,
+        BrowserAnimationsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/image-classifier/image-classifier/image-classifier.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ImageClassifierComponent } from './image-classifier.component';
+
+describe('ImageClassifierComponent', () => {
+  let component: ImageClassifierComponent;
+  let fixture: ComponentFixture<ImageClassifierComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ImageClassifierComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ImageClassifierComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
